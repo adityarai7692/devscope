31bd50ebd65e5d64fc2a017d2525fbcc6552650a
adityrai008754 <119614958+adityrai008754@users.noreply.github.com>
2026-02-17 11:41:38 +0530

    es lint error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme'); // Optional: import default themes for fallbacks
+import defaultTheme from "tailwindcss/defaultTheme";
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -10,14 +10,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'roboto': ['"Roboto"', ...defaultTheme.fontFamily.sans],
-        'poppins': ['"Poppins"', ...defaultTheme.fontFamily.sans],
-        'inter': ['"Inter"', ...defaultTheme.fontFamily.sans],
-        'space-grotesk': ['"Space Grotesk"', ...defaultTheme.fontFamily.sans],
-        'rubik': ['"Rubik"', ...defaultTheme.fontFamily.sans],
-        'work-sans': ['"Work Sans"', ...defaultTheme.fontFamily.sans],
-        'ubuntu': ['"Ubuntu"', ...defaultTheme.fontFamily.sans],
-        'open-sans': ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+        roboto: ['"Roboto"', ...defaultTheme.fontFamily.sans],
+        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        "space-grotesk": ['"Space Grotesk"', ...defaultTheme.fontFamily.sans],
+        rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
+        "work-sans": ['"Work Sans"', ...defaultTheme.fontFamily.sans],
+        ubuntu: ['"Ubuntu"', ...defaultTheme.fontFamily.sans],
+        "open-sans": ['"Open Sans"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
